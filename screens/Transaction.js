2ef35ee81d729d6c586b8d74c27b3694cdc91bd6
Nidhi Malik Kathuria <74198624+nidhimalikkathuria@users.noreply.github.com>
2022-05-26 10:58:23 +0530

    Update Transaction.js

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default class TransactionScreen extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Transaction Screen</Text>
-      </View>
-    );
-  }
-}
+
 
 const styles = StyleSheet.create({
   container: {
