e5d30a62103982be1ff77a2dcf0758b8233aafce
Nidhi Malik Kathuria <74198624+nidhimalikkathuria@users.noreply.github.com>
2022-05-26 10:57:57 +0530

    Update Search.js

--- screens/Search.js
+++ screens/Search.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default class SearchScreen extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Search Screen</Text>
-      </View>
-    );
-  }
-}
+
 
 const styles = StyleSheet.create({
   container: {
